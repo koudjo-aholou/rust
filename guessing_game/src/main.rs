863a45c5510bebe289bc83f5738bfa0005b13f22
Koudjo AHOLOU <ah.koudjo@gmail.com>
2021-02-19 22:52:05 +0100

    install library chiffre aleatoire

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,14 @@
 // io (input/output) library into scope
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Deviner le numero!");
+
+    let secret_number = rand::thread_rng().gen_range(1.. 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Svp saisissez un numero.");
     // The :: syntax in the ::new line indicates that new is an associated function of the String type.
     // associated function is implemented on a type. Create new empty string  static method.
